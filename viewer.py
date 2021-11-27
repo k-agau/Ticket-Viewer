da81715cbc1064111e364a55b8715a8a82b62fbb
k-agau <ksilvergold18@gmail.com>
2021-11-26 23:20:28 -0600

    Fleshed out more, needs work

--- viewer.py
+++ viewer.py
@@ -5,6 +5,27 @@ import click
 from pyfiglet import figlet_format
 import json
 
+# TO DO
+#   Tidy up code
+#       Page section especially
+#   Make UI look nicer
+#       FINALZIE FORMAT
+#   Error messages
+#       API unavailable / invalid response
+#           Invalid response: ticket DNE ?
+#           API unavailable: probably some way to check in pycurl/requests
+#   Testing
+#       Helper methods
+#       Main methods
+#       Make sure UI is good for all base cases
+#           Base cases:
+#   OAuth
+#       ???
+#   API: make sure everything is good / most efficient
+#   Job status stuff?
+#   Look into GET requests
+
+
 s = 'https://zccagau.zendesk.com/api/v2/'
 
 @click.group()
@@ -33,41 +54,66 @@ def log(msg, color):
 def getTicketAmount():
     return cx('tickets/count.json')
 
-def formatTickets(obj, formatType):
-    # {"error":"RecordNotFound","description":"Not found"} check for this
+def processJson(obj, type):
     x = json.loads(obj)
-    if len(x) == 2:
-        print('Ticket DNE')
+    if type not in x:
+        print("Key DNE")
         viewTicket()
-    
-    x = x["ticket"]
+    return x[type]
+
+def formatTickets(x, formatType):
     id = str(x["id"])
     subj = x["subject"]
     sent = '1'
     opened = '1'
-    # print out some important data before
-    # What would you want to see?
-    # Total number
-    # Time it took to receive?
-    # Create an enable/disable button for this
-    # Rate limit?
-    print("Ticket " + id + " with subject " + subj + ", was sent on on " + sent + " and opened on " + opened + ".")
-
+    res = "Ticket " + id + " with subject '" + subj + "' was sent on on " + sent + " and opened on " + opened + "."
+    if(len(res) > 100):
+        print(res[0:100] + "...")
+        # flesh out. add choice to expand
+        # Break up into chunks? do 100 chars first line, next 100 next line, make sure to indent + count the tabs
+    else:
+        print(res)
     if formatType:
         desc = x["description"]
-        print("Desc: " + desc) # format so x amount of characters. add button to expand/close?
+        print("Desc: " + desc) # format so x amount of characters
 
 # Main methods
 
 @cli.command(name='1')
 def viewTicket():
     choice = str(input("Choice: ")) #click.prompt('Enter a ticket number: ', type=click.Choice(str))
-    formatTickets(cx('tickets/' + choice), False)
+    if int(choice) > 0:
+        formatTickets(processJson(cx('tickets/' + choice), "ticket"), False)
     main()
 
 @cli.command(name='2')
 def viewAllTickets():
-    formatTickets(obj = cx('incremental/tickets.json?start_time=0'))
+    tickets = processJson(cx('incremental/tickets.json?start_time=0'), "tickets")
+    numTickets = len(tickets)
+    choice = 1
+    page = 0
+    while choice == 1 or choice == 2 or choice == 3:
+        pageStart = 25 * page
+        pageEnd = 25 + pageStart
+        if pageEnd > numTickets:
+            pageEnd = numTickets
+        
+        for x in range(pageStart, pageEnd):
+            formatTickets(tickets[x], False)
+        choice = int(input("1 for prev, 2 for next: "))
+
+        if choice == 1:
+            if page >= 1:
+                page -= 1
+        elif choice == 2:
+            if pageEnd != numTickets:
+                page += 1
+        elif choice == 3:
+            page = int(input("New page: "))
+            if page > numTickets / 25:
+                page = int(numTickets / 25)
+            
+        
     main()
 
 @cli.command()
@@ -93,6 +139,12 @@ def main():
     #           *** list num pages, REJECT errors over ending the program
     #       4 -> return to main menu
     
+    # print out some important data before. Could have this in the curl method?
+    # What would you want to see?
+    # Total number
+    # Time it took to receive?
+    # Create an enable/disable button for this
+    # Rate limit?
     
 
 if __name__ == '__main__':
